4d4e5964b8d8b6587a2aa86f161ead59d763d298
rahul777-oss <63468051+rahul777-oss@users.noreply.github.com>
2020-04-29 23:09:34 +0530

    Add files via upload

---
+++ operation1.cpp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ 
+ int main ()
+ {
+ 	int a;
+ 	float b;
+ 	char c;
+ 	double d;
+ 	cout<<"size of int= "<<sizeof(a)<<"bytes"<<endl;
+ 
+  	cout<<"size of float= "<<sizeof(b)<<"bytes"<<endl;
+  	
+    cout<<"size of char= "<<sizeof(c)<<"bytes"<<endl;
+	
+    cout<<"size of double= "<<sizeof(d)<<"bytes"<<endl;
+		
+	  	return 0;
+ }
---
+++ operation2.cpp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ int main ()
+ {
+ 	float a,b,product;
+ 	cout<<"enter the first number: ";
+ 	cin>>a;
+ 	cout<<"enter the sceand number: ";
+ 	cin>>b;
+ 	
+	 product=a*b;
+ 	
+	cout<<" product is :"<<product;
+	return 0;
+ }
+
